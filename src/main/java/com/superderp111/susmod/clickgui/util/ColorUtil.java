2273c89d37b5e0e04afdd20ea85df5c43f5a2ec2
VeryHolyCheeeese <79263404+VeryHolyCheeeese@users.noreply.github.com>
2022-07-31 14:48:22 +0800

    made colorutil actually readable

--- src/main/java/com/superderp111/susmod/clickgui/util/ColorUtil.java
+++ src/main/java/com/superderp111/susmod/clickgui/util/ColorUtil.java
@@ -7,6 +7,8 @@ import java.awt.*;
 public class ColorUtil {
 
     public static Color getClickGUIColor(){
-        return new Color((int) SusMod.settingsManager.getSettingByName("Color Red").getValDouble(), (int)SusMod.settingsManager.getSettingByName("Color Green").getValDouble(), (int)SusMod.settingsManager.getSettingByName("Color Blue").getValDouble());
+        return new Color((int) SusMod.settingsManager.getSettingByName("Color Red").getValDouble(),
+                (int)SusMod.settingsManager.getSettingByName("Color Green").getValDouble(),
+                (int)SusMod.settingsManager.getSettingByName("Color Blue").getValDouble());
     }
 }
